8ffd11c217df701db2d34e628285400986369996
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2022-07-04 21:53:08 +0530

    bug 2

--- src/main/java/com/aws/sample/AwsTestApplication.java
+++ src/main/java/com/aws/sample/AwsTestApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = TestController.class)
 public class AwsTestApplication {
 
 	public static void main(String[] args) {
