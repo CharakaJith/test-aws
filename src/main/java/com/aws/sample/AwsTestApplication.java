abb7a6f1bd80099f1ee9c35b5b963368169e5d80
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2022-07-04 21:50:01 +0530

    bug

--- src/main/java/com/aws/sample/AwsTestApplication.java
+++ src/main/java/com/aws/sample/AwsTestApplication.java
@@ -1,9 +1,12 @@
 package com.aws.sample;
 
+import com.aws.sample.Controller.TestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = TestController.class)
 public class AwsTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/aws/sample/Controller/TestController.java
+++ src/main/java/com/aws/sample/Controller/TestController.java
@@ -1,12 +1,17 @@
 package com.aws.sample.Controller;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/test")
+@RequestMapping("/")
 public class TestController {
+    @GetMapping()
     public String testEndpoint(){
+        System.out.println("testEndpoint was hit.....");
+
         return "Hello world!";
     }
 }
